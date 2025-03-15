e6c3711cf4818fcd4a310019bb623536f93f3d6f
12araa <radhikaprema34@gmail.com>
2025-03-15 12:03:51 +0800

    Upload Project 2

--- pertemuan2/src/pertemuan2/Pertemuan2.java
+++ pertemuan2/src/pertemuan2/Pertemuan2.java
@@ -4,9 +4,15 @@
  */
 package pertemuan2;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 /**
  *
- * @author INSTIKI
+ * @author Radhika
+ * Tgl : 15 Maret 2025
+ * 
  */
 public class Pertemuan2 {
 
@@ -14,7 +20,28 @@ public class Pertemuan2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String nama = "Radhika";
+        String nim = "2301010068";
+        double ipk = 3.75;  
+        
+        BufferedReader dtain = new BufferedReader(new InputStreamReader(System.in));
+        
+        System.out.print("Input Nama Mahasiswa: ");
+        try{
+            nama = dtain.readLine();
+            System.out.print("Input NIM Mahasiswa: ");
+            nim = dtain.readLine();
+            System.out.print("Input IPK Mahasiswa: ");
+            ipk = Double.parseDouble(dtain.readLine());
+            
+        }catch(IOException e){
+            System.out.println("Error pembacaan data");
+        }
+        
+//        System.out.println(nama);
+//        System.out.println(nim);
+        
+        System.out.printf("Nama saya %s dengan NIM %s dengan IPK %.2f ", nama, nim, ipk);
     }
     
 }
